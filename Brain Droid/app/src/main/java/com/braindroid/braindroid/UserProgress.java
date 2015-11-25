e99cba9fd38245de8c11d66a5f906192b4b63c87
Camilo Moreno <cmcamilomoreno@gmail.com>
2015-11-25 01:07:20 -0500

    Account and Reports Pages
    - finished account page layout
    - began on progress report page

--- Brain Droid/app/src/main/java/com/braindroid/braindroid/UserProgress.java
+++ Brain Droid/app/src/main/java/com/braindroid/braindroid/UserProgress.java
@@ -1,23 +1,54 @@
 package com.braindroid.braindroid;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by Camilo on 11/8/2015.
  */
 public class UserProgress extends AppCompatActivity{
+
+    private View mathT, memoryT, vocabT, reportLay;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_progress);
 
+        mathT = (TextView) findViewById(R.id.mathTab);
+        mathT.setBackgroundColor(Color.rgb(197,208,241));
+        memoryT = (TextView) findViewById(R.id.memoryTab);
+        vocabT = (TextView) findViewById(R.id.vocabTab);
+        reportLay = (LinearLayout) findViewById(R.id.reportLay);
+
         final Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+    }
+
+    public void tab(View v){
+        mathT.setBackgroundColor(Color.rgb(33,36,217));
+        memoryT.setBackgroundColor(Color.rgb(33,36,217));
+        vocabT.setBackgroundColor(Color.rgb(33,36,217));
+        switch (v.getId()){
+            case R.id.mathTab:
+                mathT.setBackgroundColor(Color.rgb(197,208,241));
+                break;
+            case R.id.memoryTab:
+                memoryT.setBackgroundColor(Color.rgb(197,208,241));
+                break;
+            case R.id.vocabTab:
+                vocabT.setBackgroundColor(Color.rgb(197,208,241));
+                break;
+            default:
+        }
     }
 
     @Override
